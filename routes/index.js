0f3c12c5459fa61646f7d513ae5e591af6e17c42
Parris Khachi <parris@letswoosh.com>
2013-02-16 13:02:29 -0800

    Route for instrument pages

--- routes/index.js
+++ routes/index.js
@@ -12,3 +12,7 @@ exports.index = function(req, res){
 	midiManager.sendNote();
     res.render('index', { title: 'Let\'s Jam' });
 };
+
+exports.html = function(req, res) {
+    res.render('instruments/' + req.params.file);
+};
