a2448a61ed2145bf70bfb3b3232d5429a1ba2b1c
Parris Khachi <parris@letswoosh.com>
2013-02-17 10:46:21 -0800

    added styles

--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -6,9 +6,21 @@
 
     $(function() {
         var socket = io.connect('http://localhost:3000'),
-            instrument;
+            instrument,
+            worldClock,
+            bpm,
+            wholeNoteDelay,
+            metronomeWidth,
+            lastTime = 0,
+            timeSinceLastMetronomeClick = 0,
+            quarterNoteDelay = 0,
+            requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-        // TODO: remember to read current socket state, page refresh != new connection
+        window.requestAnimationFrame = requestAnimationFrame;
+
+        // TODO: remember to read current socket state
+        // page refresh != new connection
         socket.on('assign', function(data) {
             instrument = data.instrument;
             getInstrument(instrument).done(function(response){
@@ -33,6 +45,38 @@
         });
 
 
+        function setMetronomeWidth(width) {
+            metronomeWidth = width;
+        }
+
+        /*function renderMetronome(timeStamp) {
+            var delta = timeStamp - lastTime;
+            timeSinceLastMetronomeClick += delta;
+
+            if (timeSinceLastMetronomeClick > wholeNoteDelay) {
+                // click logic here
+                timeSinceLastMetronomeClick = 0;
+            }
+
+            // animation logic here
+
+            lastTime = timeStamp;
+            requestAnimationFrame(renderMetronome);
+        }*/
+
+        socket.on('ready', function(data) {
+            worldClock = data.clock;
+            bpm = data.bpm;
+            quarterNoteDelay = Math.round(((60/bpm)*1000)*100000)/100000;
+            wholeNoteDelay = quarterNoteDelay * 4;
+            //requestAnimationFrame(renderMetronome);
+        });
+
+        $(window).on('resize', function(e) {
+            setMetronomeWidth($(e.target).width());
+        });
+
+        setMetronomeWidth($(window).width());
     });
 
 }());
